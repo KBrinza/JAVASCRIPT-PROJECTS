d38c832c570f720ea822d8c784255c9c0a3cad09
KBrinza <KellyBrinza@gmail.com>
2024-11-08 16:14:16 -0800

    Adding Project1_expressions_alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/JavaScript_File.js
@@ -0,0 +1,42 @@
+//WINDOW ALERTS
+window.alert("Hello World!");
+
+//LEARNING VARIABLES//
+document.write("Hello World! Written with JS.");
+
+var A = " This is a string.";
+document.write(A);
+
+//WINDOW ALERTS VIA VARIABLES
+var B = "New window alert!";
+window.alert(B);
+
+//ESCAPING CHARACTERS//
+document.write(" Janet\'s dog\'s name is Fluffy");
+document.write("\ “Be who you are and say what you feel,"
+    + " because those who mind don\'t matter and those who matter don\'t mind.\”"
+    + "Dr. Seuss");
+
+//CONCATENATE CHALLENGE//
+var C = "\ “TEST TEST,"
+    + " THIS ISN\'T MY CONCATENATE \"CHALLENGE.\”"
+    + " BUT MY \"Assign a concatenated string value to a variable\" CHALLENGE!";
+document.write(C)
+
+//MULTIPLE VARIABLE//
+var D = " The dogs", E = " The cats", F = " The birds", G = " The fish";
+document.write(G);
+
+//EXPRESSIONS//
+document.write(3 + 3);
+
+//**** BASIC JAVASCRIPT PROJECTS SUBMISSION ASSIGNMENT ****//
+//window alert//
+window.alert("This begins my Basic JavaScript Projects and Submission Assignment")
+
+//variables//
+var Sent1 = "****This is the beginning of the string****";
+var Sent2 = "****and this is the end of the string****";
+
+//Concatenate and Expression combo!//
+document.write(Sent1 + Sent2)
